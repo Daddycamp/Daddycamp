2a617cc554d066e9d0b949be7b20234fd1e2c540
Daddycamp <loffz@gmx.de>
2026-05-19 19:52:41 +0200

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
-  plugins:[react()],
-  base:'/',
-  build:{chunkSizeWarningLimit:2000}
+  plugins: [react()],
+  base: './',
+  build: { chunkSizeWarningLimit: 3000 },
 })
